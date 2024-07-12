3e9a15d29a94d4f22213a081bf79682b5f5b9fef
Nathan <urwin.nathan@gmail.com>
2024-07-12 12:01:04 +0100

    add thoughts on shortestMove func

---
+++ shortestMove.js
@@ -0,0 +1,13 @@
+/**
+ * A knight can get from any square of the board to any other
+ * in a maximum of 6 moves, therefore check if it can be done in
+ * x moves, with x increasing from 0 to 6.
+ */
+/**
+ * Build a binary search tree using the adjacency list
+ * for every node, the valid moves have an x and y coordinate
+ * There can be between 2 and 8 valid moves for a knight
+ * This means no binary tree - they can only decide between two options
+ * it would be very expensive to build a list of the optimal path from
+ * each node to every other node (64 factorial??)
+ */
