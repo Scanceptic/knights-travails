c3776b669c575686f5787043b5cc65fa17978f51
Nathan <urwin.nathan@gmail.com>
2024-07-12 10:02:48 +0100

    create renderMove func

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ function knightMoves() {
 	// edge list? adjacency matrix? adjacency list?
 	// build adjacency list for each of the 64 possible start positions
 	const adjacencyList = [];
-	console.log(adjacencyList);
 	for (let i = 0; i < 8; i++) {
 		const adjacencyRow = [];
 		for (let j = 0; j < 8; j++) {
@@ -27,15 +26,38 @@ function knightMoves() {
 			]);
 		}
 		adjacencyList.push(adjacencyRow);
-		console.log(adjacencyList);
 		// TODO: check valid moves to push
 	}
 	// all possible grids and their relationship to each other FROM THE PERSPECTIVE OF THE KNIGHT MOVES (L shape)
 	// then find the shortest distance to endX endY
 	// this is the quickest set of moves
-	return adjacencyList;
+	// checks move is valid
+	function move(startX, startY, endX, endY) {
+		// if move array position startX and startY includes an element with [endX, endY]
+		// && if neither start or end position are above 7 or below 0 (out of board)
+		if (
+			(adjacencyList[startX][startY].includes([endX, endY]) === true && startX,
+			startY,
+			endX,
+			endY >= 0 && startX,
+			startY,
+			endX,
+			endY < 8)
+		) {
+			// remove old position
+			//adjacencyList[startX][startY]
+			// add new position
+			//adjacencyList[endX][endY]
+			// move is legal, return move coords
+			return endX, endY;
+		} else {
+			// else return false - invalid move
+			return false;
+		}
+	}
+	return { adjacencyList, move };
 }
-
+// creates board on DOM
 function createBoard() {
 	const board = document.getElementById("board");
 	for (let i = 0; i < 8; i++) {
@@ -47,8 +69,11 @@ function createBoard() {
 		}
 	}
 }
-
-function renderBoard() {
+// takes valid move from move func and renders on DOM
+function renderMove(moveX, moveY) {
 	const board = document.getElementById("board");
 	const squares = document.querySelectorAll("square");
+	const moveSquare = moveX * 8 + moveY;
+	const moveSquareDOM = document.getElementById(`${moveSquare}`);
+	moveSquareDOM.textContent = "Knight";
 }
