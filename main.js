43426f7cfa46451027374ac5d6f507a1c5776ec9
Nathan <urwin.nathan@gmail.com>
2024-07-12 11:39:43 +0100

    add onclick square moves

--- main.js
+++ main.js
@@ -21,32 +21,40 @@ function createKnightMoves() {
 	}
 	// checks move is valid
 	function move(oldPosition, newPosition) {
+        /*
 		console.log("Current position is:");
 		console.log(oldPosition);
 		console.log("Submitted Move is:");
 		console.log(newPosition);
+        */
 		// get all moves from current position
 		const possibleMoves = adjacencyList[oldPosition[0]][oldPosition[1]];
+        /*
 		console.log("Possible moves at current position:");
 		console.log(possibleMoves);
+        */
 		// filter for moves within the board
 		const validMoves = possibleMoves.filter(
 			(move) => move[0] >= 0 && move[0] <= 7 && move[1] >= 0 && move[1] <= 7
 		);
+        /*
 		console.log("valid moves at current position:");
 		console.log(validMoves);
+        */
 		// if move array position startX and startY includes an element with [endX, endY]
 		// && if neither start or end position are above 7 or below 0 (out of board)
 		for (let i = 0; i < validMoves.length; i++) {
-			//console.log(`Move Attempt ${i}:`);
-			//console.log(newPosition);
+			/*
+            console.log(`Move Attempt ${i}:`);
+			console.log(newPosition);
 			console.log("Valid Move to check:");
 			console.log(validMoves[i]);
+            */
 			if (
 				validMoves[i][0] === newPosition[0] &&
 				validMoves[i][1] === newPosition[1]
 			) {
-				console.log("Valid Move");
+				//console.log("Valid Move");
 				// update old Position
 				oldPosition = newPosition;
 				// move is legal, return move coords
@@ -54,7 +62,7 @@ function createKnightMoves() {
 			}
 		}
 		// else return false - invalid move
-		console.log("Invalid move");
+		//console.log("Invalid move");
 		return false;
 	}
 	// renders board on DOM
@@ -70,6 +78,20 @@ function createKnightMoves() {
 				const square = document.createElement("div");
 				square.classList.add("square");
 				square.id = i * 8 + j;
+				// allow click to move
+				square.addEventListener("click", () => {
+					const position = move(oldPosition, [i, j - 1]);
+					if (position) {
+						// update old position
+						oldPosition = position[0];
+						//console.log("Old Position:");
+						//console.log(oldPosition);
+						// render new position
+						renderBoard(position[1]);
+					} else {
+						//console.log("Move was Invalid, try again");
+					}
+				});
 				squares.appendChild(square);
 				if (
 					position &&
@@ -98,11 +120,11 @@ submitButton.addEventListener("click", () => {
 	if (position) {
 		// update old position
 		oldPosition = position[0];
-		console.log("Old Position:");
-		console.log(oldPosition);
+		//console.log("Old Position:");
+		//console.log(oldPosition);
 		// render new position
 		knightMoves.renderBoard(position[1]);
 	} else {
-		console.log("Move was Invalid, try again");
+		//console.log("Move was Invalid, try again");
 	}
 });
