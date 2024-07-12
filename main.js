a09734ad0eec95656aa85864d39ceb039050ffc7
Nathan <urwin.nathan@gmail.com>
2024-07-12 09:13:45 +0100

    change planned method

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+// establish rules of board
+// dont go beyond the edges (7x7)
+// can only go to [x+/-1, y+/-2] or [x+/-2, y+/-1] valid moves for knight
+// for every square, there is a number of possible moves, choose a data structure that will accomodate this
+// which search algorithm is best to use for this
+// use the search algorithm to find the shortest path between the start and end
+// output the full path - every node stopped at along the way
 function knightMoves([startX, startY], [endX, endY]) {
-	// establish rules of board
-	// dont go beyond the edges (7x7)
-	// can only go to [x+/-1, y+/-2] or [x+/-2, y+/-1] valid moves for knight
-	// for every square, there is a number of possible moves, choose a data structure that will accomodate this
-	// which search algorithm is best to use for this
-	// use the search algorithm to find the shortest path between the start and end
-	// output the full path - every node stopped at along the way
+	// build a graph of all possible moves from [startX startY] to [endX endY]?
+	// then find the shortest distance to endX endY
+	// this is the quickest set of moves
 }
