5134a7399cea14bdf5c8b5a10b5c81ac88153fff
Nathan <urwin.nathan@gmail.com>
2024-07-12 09:34:10 +0100

    add initial adjacencyList creation func

--- main.js
+++ main.js
@@ -7,6 +7,26 @@
 // output the full path - every node stopped at along the way
 function knightMoves([startX, startY], [endX, endY]) {
 	// build a graph of all possible moves from [startX startY] to [endX endY]?
+	// edge list? adjacency matrix? adjacency list?
+	// build adjacency list for each of the 64 possible start positions
+	const adjacencyList = [];
+	for (let i = 0; i < 8; i++) {
+		for (let j = 0; j < 8; j++) {
+            // push all valid moves from that square
+			adjacencyList[i][j].push(
+				[i + 1, j + 2],
+				[i - 1, j + 2],
+				[i - 1, j - 2],
+				[i + 1, j - 2],
+				[i + 2, j + 1],
+				[i + 2, j - 1],
+				[i - 2, j + 1],
+				[i - 2, j - 1]
+			);
+		}
+		// TODO: check valid moves to push
+	}
+	// all possible grids and their relationship to each other FROM THE PERSPECTIVE OF THE KNIGHT MOVES (L shape)
 	// then find the shortest distance to endX endY
 	// this is the quickest set of moves
 }
