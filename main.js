f11e6e205b340cfc3ba5634a07fab617323a4fa4
Nathan <urwin.nathan@gmail.com>
2024-07-12 09:01:38 +0100

    add function outline

---
+++ main.js
@@ -0,0 +1,9 @@
+function knightMoves([startX, startY], [endX, endY]) {
+	// establish rules of board
+	// dont go beyond the edges (7x7)
+	// can only go to [x+/-1, y+/-2] or [x+/-2, y+/-1] valid moves for knight
+	// for every square, there is a number of possible moves, choose a data structure that will accomodate this
+	// which search algorithm is best to use for this
+	// use the search algorithm to find the shortest path between the start and end
+	// output the full path - every node stopped at along the way
+}
